c023a90ff2afe4c872ab36f01e598c0a1887bf28
OutlyingAce <regmisiddhant1@gmail.com>
2023-11-27 17:59:04 +0545

    27th Novemver

--- src/Main.java
+++ src/Main.java
@@ -3,27 +3,28 @@ import Models.Student;
 
 public class Main {
     public static void main(String[] args) {
-//        Student st = new Student();
-//        System.out.println(st);
-//
-//        Student su = new Student();
-//        System.out.println(su);
-//
-//        System.out.println(st.getAge());
-//        System.out.println(su.getName());
+        Student st = new Student();
+        System.out.println(st);
+        Student su = new Student();
+        System.out.println(su);
+        System.out.println(st.getAge());
+        System.out.println(su.getName());
 
-//        Section de = new Section();
-////        System.out.println(de.getName());
-////        System.out.println(de.getRoom_Number());
-////        System.out.println(de.getCollege());
-////        de.setName("Siddhant Regmi");
-////        System.out.println(de.getName());
-////        de.setRoom_Number("12345");
-////        de.setName("Ram");
-////        de.setCollege("Chelsea");
-////        System.out.println(de.getName());
-////        System.out.println(de.getRoom_Number());
-////        System.out.println(de.getCollege());
+
+        Section de = new Section();
+        System.out.println(de.getName());
+        System.out.println(de.getRoom_Number());
+        System.out.println(de.getCollege());
+        de.setName("Siddhant Regmi");
+        System.out.println(de.getName());
+        de.setRoom_Number("12345");
+        de.setName("Ram");
+        de.setCollege("Chelsea");
+        System.out.println(de.getName());
+        System.out.println(de.getRoom_Number());
+        System.out.println(de.getCollege());
+
+
         Section si = new Section();
         System.out.println(si.getName());
         System.out.println(si.getRoom_Number());
